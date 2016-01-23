5929c7d6383ae1db96a4a545b08abd6e95ad2e60
ngenter <njg401@yahoo.com>
2016-01-23 18:33:08 -0500

    file for caleb

---
+++ chal_randa.py
@@ -0,0 +1,26 @@
+# Chapter 5 Challenge.
+# Create a program that prints a list of words in random order.
+# The program should print all the words and not repeat any.
+
+# import random
+# create a word list
+# print a random word
+# delete the printed random word
+# loop until all words have been printed
+
+import random
+
+wl = ["annilox", "plates," "birdie", "chamber", "nip", "gate"]
+randword = random.choice(wl)
+
+print("Word list is", wl)
+
+
+while wl != ():
+    print(randword)
+    del 
+    
+
+
+input("\nPress the enter key to exit.")
+      
